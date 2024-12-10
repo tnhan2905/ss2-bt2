a8f62d624866c24d6a0e19248c9dc3b3da1f6d7c
tnhan2905 <diepthanhnhan2019.st@gmail.com>
2024-12-10 15:45:01 +0700

    Add files via upload

---
+++ b2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 55;
+    int b = 855;
+
+    int sum;
+
+    sum = a + b;
+
+    printf("Tong cua %d và %d là: %d\n", a, b, sum);
+
+    return 0;
+}
+
